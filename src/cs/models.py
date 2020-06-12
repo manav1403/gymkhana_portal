e538269d03f0083cf7e10cb7f6db0e1387786b68
manav1403 <manavavni@gmail.com>
2020-06-12 21:25:55 +0530

    added app for cs_site

---
+++ src/cs/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+import datetime
+
+
+class Team(models.Model):
+    TEAM_CHOICES = (
+        ('UG', 'UG'),
+        ('PG', "PG"),
+    )
+    YEAR_CHOICES = [(x,x) for x in range(int(datetime.datetime.now().year), 2000, -1)]
+    
+    year = models.CharField(max_length=10, choices= YEAR_CHOICES, unique=True)
+    team_type = models.CharField(max_length=4, choices=TEAM_CHOICES)
+    members = models.ManyToManyField("oauth.UserProfile")
+
+    def __str__(self):
+        return self.team_type + self.year
